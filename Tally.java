446036da5a1e06024cec118740c8c31c851459b9
klthree <klthree@users.noreply.github.com>
2018-06-15 01:58:24 -0400

    Update Tally.java

--- Tally.java
+++ Tally.java
@@ -1,3 +1,8 @@
+/**
+ * https://www.reddit.com/r/dailyprogrammer/comments/8jcffg/20180514_challenge_361_easy_tally_program/
+ * tally three players scores and sort
+ */
+
 import java.util.Scanner;
 
 public class Tally {
